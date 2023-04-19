95ffc409c4ac0f04d81d4b3ebfee4329df7113fb
yupingping <paopao0108@outlook.com>
2023-04-19 22:06:22 +0800

    third commit

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,6 +11,9 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("xxx");
+            Console.WriteLine("xxx");
+            Console.WriteLine("yyy");
+
         }
     }
 }
